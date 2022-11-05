99b56d89956455233eb01fad63728338a7219bf3
ian-carlos <iancarlos335@gmail.com>
2022-11-04 22:04:50 -0300

    Deixando com as classes iguais, pra n dar conflito com o angular

--- src/main/java/com/lanchonete/controller/ClientesController.java
+++ src/main/java/com/lanchonete/controller/ClientesController.java
@@ -25,7 +25,7 @@ import com.lanchonete.repository.ClienteRepository;
 
 @RestController
 @CrossOrigin(origins="*", maxAge=3600)
-@RequestMapping("/clientes")
+@RequestMapping("/cliente")
 public class ClientesController {
 	
 
--- src/main/java/com/lanchonete/repository/ClienteRepository.java
+++ src/main/java/com/lanchonete/repository/ClienteRepository.java
@@ -3,9 +3,9 @@ package com.lanchonete.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lanchonete.model.Cliente;
+import com.lanchonete.model.Clientes;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Clientes, Long> {
 
 }
